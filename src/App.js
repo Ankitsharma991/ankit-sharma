24cc672466f0abbee10b1d0b7c759b37d0915ad4
Ankitsharma991 <sharmaa7861@gmail.com>
2024-02-05 22:36:43 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" Component={Home} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="w-full flex justify-center mt-5">
+      <h1 className="font-bold text-[22px]">Ankit Sharma</h1>
+    </div>
+  );
+};
+
+export default Home;
